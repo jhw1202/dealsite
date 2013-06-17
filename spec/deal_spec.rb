61387f58c5c15024a524f34f84b90635d6a30646
Philip Woo <jhw1202@gmail.com>
2013-06-16 21:11:04 -0700

    stuff for testing. need to start testing.

---
+++ spec/deal_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Deal do
+
+  context "validations" do
+    it "should validate presence "
+  end
+
+end
---
+++ spec/factories/deals.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :deal do
+  title "Testing this deal"
+  body "This deal is awesome. Free!"
+  source "http://www.amazon.com/PlayStation-4-Launch-Day-Edition/dp/B00BGA9WK2/ref=sr_1_1?ie=UTF8&qid=1371260904&sr=8-1&keywords=playstation+4"
+  end
+end
+
