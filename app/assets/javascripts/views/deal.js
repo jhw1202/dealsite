25f636f5a187fc5a0d7bcb44d89d8f214e3549e0
Philip Woo <jhw1202@gmail.com>
2013-06-18 14:34:39 -0700

    wip update deal submission

--- app/assets/javascripts/views/deal.js
+++ app/assets/javascripts/views/deal.js
@@ -45,7 +45,8 @@ App.Views.SubmitDealForm = Backbone.View.extend({
     el: "#dialog-form",
 
     events: {
-      'submit .deal-submit-form' : 'submitDeal'
+      'submit .deal-submit-form' : 'submitDeal',
+      'submit .deal-edit-form' : 'updateDeal'
     },
     //try to use this for create and update
     submitDeal: function(event){
@@ -67,11 +68,9 @@ App.Views.SubmitDealForm = Backbone.View.extend({
     },
 
     editFormRender: function(options){
-      console.log("HERERHEIRHEIRHIEHIRH")
       this.deal = new App.Models.Deal({id: options.id})
       this.deal.fetch({
         success: function(deal){
-          console.log(deal)
           var dealData = deal.attributes
           $('.dummy-div').html(template("edit-deal-form", {deal: dealData}))
         }
@@ -79,14 +78,15 @@ App.Views.SubmitDealForm = Backbone.View.extend({
     },
 
     updateDeal: function(event){
+      console.log("HEEEHRHERHEs")
       event.preventDefault()
       var dealData = $(event.currentTarget).serializeObject()
       console.log(dealData.id)
-      var deal = new App.Models.Deal()
-      deal.save(dealData, {
-        success: function(deal){
-          console.log(deal.id)
-        }
-      })
+      // var deal = new App.Models.Deal()
+      // deal.save(dealData, {
+      //   success: function(deal){
+      //     console.log(deal.id)
+      //   }
+      // })
     }
   })
