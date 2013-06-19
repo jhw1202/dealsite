34eedbc5382505919ef4dbd5dd92c4dadd2b2055
Chris Malin <csmalin@gmail.com>
2013-06-19 12:41:42 -0700

    hide loader/unhide .deals on single object view

--- app/assets/javascripts/views/deal.js
+++ app/assets/javascripts/views/deal.js
@@ -35,8 +35,10 @@ App.Views.Deal = Backbone.View.extend({
     this.model = new App.Models.Deal({id: options.id})
     this.model.fetch({
       success: function(deal){
+        $('#ajax-loader').css({"visibility": "hidden"})
         var data = {deal: deal.attributes}
         _this.$el.html(template('single-deal-template', data))
+        $('.deals').css({"visibility": "visible"})
       }
     })
     return this
